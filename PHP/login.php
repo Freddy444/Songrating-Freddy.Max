0b17da550429408dd4e46635952206a7f557f8b7
Freddy Fabian <freddy.f0116@gmail.com>
2023-10-17 00:16:05 -0400

    logged in user indicated at top

--- PHP/login.php
+++ PHP/login.php
@@ -1,6 +1,15 @@
 <?php 
     require_once "db.php";
 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    #testing connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        echo " $username Connected successfully";
+    }
 
     session_start(); 
     if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
