f78792d4f69d87dfbe9c1c9704e3d9ceab33cb6b
Freddy Fabian <freddy.f0116@gmail.com>
2023-10-16 23:51:32 -0400

    created login and registration page

--- PHP/db.php
+++ PHP/db.php
@@ -4,14 +4,4 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "music_db";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-#testing connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else {
-    echo "Connected successfully";
-}
 ?>
