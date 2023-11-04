29d50bd5b0b5baae5ee934c0a604a0097aa90e6e
Freddy Fabian <99642629+Freddy444@users.noreply.github.com>
2023-11-04 04:33:48 -0400

    Update registerView.js

--- songratingfront/frontend/src/views/registerView.js
+++ songratingfront/frontend/src/views/registerView.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { TextField, Button } from "@mui/material";
 import axios from "axios";
-import styles from "./registerView.module.css";
+import styles from "./register.module.css";
 import { useNavigate } from "react-router-dom";
 
 export default function RegisterView() {
