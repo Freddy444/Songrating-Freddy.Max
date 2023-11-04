505920117eb937ead0c49db34092be492e7a0754
Freddy Fabian <freddy.f0116@gmail.com>
2023-11-04 01:04:55 -0400

    bugs

--- songratingfront/frontend/src/src/nav.js
+++ songratingfront/frontend/src/src/nav.js
@@ -13,9 +13,9 @@ function NavigationBar() {
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/music">Rate A Song</Nav.Link>
                         <Nav.Link href="/favorites">Favorites</Nav.Link>
-                        <Nav.Link href="/login">Login</Nav.Link>
                         <NavDropdown title="Account" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/newlogin">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+                            <NavDropdown.Item href="/newsignup">Sign up</NavDropdown.Item>
                             <NavDropdown.Item href="/">Logout</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
